05d246eb1c79f6f6dc05c57d773c75ef6b5fb642
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-07-24 20:08:12 -0700

    Task 0

---
+++ 0-_printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format for chars and strings.
+ * @format: character string
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list allArgs;
+	unsigned int i;
+	char *str;
+
+	va_start(allArgs, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				_putchar(va_arg(allArgs, int));
+				i++;
+			}
+			else if (*format == 's')
+			{
+				char *str = (va_arg(allArgs, char*));
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					str++;
+					i++;
+				}
+			
+			}
+			else if (*format == '%')
+			{
+				_putchar(va_arg(allArgs, int));
+				i++;
+			}
+			else
+			{
+			 	_putchar(*format);
+				i++;
+			}
+		}
+		format++;
+	}
+	va_end(allArgs);
+	return (i);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
