8d7dfa681b0b7a9f8cd95976e49a8bddc0ea620f
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-07-25 14:09:11 -0700

    print_rot

---
+++ print_rot13/_strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ ** _strcpy - copies the string pointed to by src, including the
+ *terminating null byte, to the buffer pointed to by dest.
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to i.
+ *
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
+			break;
+		i++;
+	}
+	return (dest);
+}
